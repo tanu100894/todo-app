bc94b3d881df22bb0405ccce5ff7c51b375e0fad
Tanu Agarwal <116576884+tanu100894@users.noreply.github.com>
2025-05-10 14:15:44 +0200

    Implement the theme, clock, Button through image and also do the error handling in Desktop GUI.

--- cli.py
+++ cli.py
@@ -5,55 +5,62 @@ import time
 now = time.strftime("%b %d, %Y %H:%M:%S")
 print("It is", now)
 
-while True:
-    user_action = input("Type add, show, edit, complete or exit: ")
-    user_action = user_action.strip()  # For striping out the blank space
+try:
+    while True:
+        user_action = input("Type add, show, edit, complete or exit: ")
+        user_action = user_action.strip()  # For striping out the blank space
 
-    if user_action.startswith('add'):
-        todo = user_action[4:]
-        todos = functions.get_todos()
-        todos.append(todo + '\n')
-        functions.write_todos(todos)
+        if user_action.startswith('add'):
+            todo = user_action[4:]
+            if len(todo) > 0:
+                todos = functions.get_todos()
+                todos.append(todo + '\n')
+                functions.write_todos(todos)
+            else:
+                print("Please enter a valid todo")
 
-    elif user_action.startswith('show'):
-        todos = functions.get_todos()
-
-        for index, item in enumerate(todos):  # adds a counter to each item in a list or other iterable.
-            item = item.strip('\n').title()
-            row = f"{index + 1}. {item}"  # For defining f-string
-            print(row)
-
-    elif user_action.startswith('edit'):
-        try:
-            number = int(user_action[5:])
-            number = number - 1
+        elif user_action.startswith('show'):
             todos = functions.get_todos()
-            todos[number] = input("Enter the new todo: ") + '\n'
-            functions.write_todos(todos)
 
-        except ValueError:
-            print("Your command is not valid.")
-            continue
+            for index, item in enumerate(todos):  # adds a counter to each item in a list or other iterable.
+                item = item.strip('\n').title()
+                row = f"{index + 1}. {item}"  # For defining f-string
+                print(row)
 
-    elif user_action.startswith('complete'):
-        try:
-            number = int(user_action[9:])
-            todos = functions.get_todos()
-            index = number - 1
-            todo_to_remove = todos[index].strip('\n')
-            todos.pop(index)
-            functions.write_todos(todos)
-            message = f"Todo '{todo_to_remove}' was removed from the list."
-            print(message)
+        elif user_action.startswith('edit'):
+            try:
+                number = int(user_action[5:])
+                number = number - 1
+                todos = functions.get_todos()
+                todos[number] = input("Enter the new todo: ") + '\n'
+                functions.write_todos(todos)
 
-        except IndexError:
-            print("There is no item with that number.")
-            continue
+            except ValueError:
+                print("Your command is not valid.")
+                continue
 
-    elif user_action.startswith('exit'):
-        break
+        elif user_action.startswith('complete'):
+            try:
+                number = int(user_action[9:])
+                todos = functions.get_todos()
+                index = number - 1
+                todo_to_remove = todos[index].strip('\n')
+                todos.pop(index)
+                functions.write_todos(todos)
+                message = f"Todo '{todo_to_remove}' was removed from the list."
+                print(message)
 
-    else:
-        print("Command is not Valid")
+            except IndexError:
+                print("There is no item with that number.")
+                continue
+
+        elif user_action.startswith('exit'):
+            break
+
+        else:
+            print("Command is not Valid")
+
+except KeyboardInterrupt:
+    print("\nProgram interrupted by user.")
 
 print("Bye!")
--- gui.py
+++ gui.py
@@ -1,53 +1,69 @@
 import functions
 import FreeSimpleGUI as sg
+import time
 
+sg.theme("Black")
+clock = sg.Text('', key="clock")
 label = sg.Text("Type in a to-do")
-input_box = sg.InputText(tooltip="Enter todo", key='todo')
-add_button = sg.Button("Add")
-list_box = sg.Listbox(values=functions.get_todos(), key='todos',
+input_box = sg.InputText(tooltip="Enter todo", key="todo")
+add_button = sg.Button(size=3, image_source="add.png", mouseover_colors="LightBlue2", tooltip="Add Todo", key="Add")
+list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=[45,10])
 edit_button = sg.Button("Edit")
-complete_button = sg.Button("Complete")
+complete_button = sg.Button(size=3, image_source="complete.png", mouseover_colors="LightBlue2", tooltip="Complete Todo", key="Complete")
 exit_button = sg.Button("Exit")
 
 window = sg.Window('My To-Do App',
-                   layout=[[label],
+                   layout=[[clock],
+                           [label],
                            [input_box, add_button],
                            [list_box, edit_button, complete_button],
                            [exit_button]],
                    font=('Helvetica', 15))
 
 while True:
-    event, values = window.read()
-    print(event)
-    print(values)
+    event, values = window.read(timeout=200)
+
+    if event == sg.WIN_CLOSED:
+        break
+
+    window['clock'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
+
     match event:
         case "Add":
             todos = functions.get_todos()
             new_todo = values['todo'] + "\n"
-            todos.append(new_todo)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            if len(new_todo) > 0:
+                todos.append(new_todo)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            else:
+                sg.popup("Please enter the todo first", font=('Helvetica', 15))
         case "Edit":
-            todo_to_edit = values['todos'][0]
-            new_todo = values['todo'] + '\n'
-
-            todos = functions.get_todos()
-            index = todos.index(todo_to_edit)
-            todos[index] = new_todo
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
+            try:
+                todo_to_edit = values['todos'][0]
+                new_todo = values['todo'] + '\n'
+                todos = functions.get_todos()
+                index = todos.index(todo_to_edit)
+                todos[index] = new_todo
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+            except IndexError:
+                sg.popup("Please select the item first", font=('Helvetica', 15))
         case "Complete":
-            todo_to_complete = values['todos'][0]
-            todos = functions.get_todos()
-            todos.remove(todo_to_complete)
-            functions.write_todos(todos)
-            window['todos'].update(values=todos)
-            window['todo'].update(value='')
+            try:
+                todo_to_complete = values['todos'][0]
+                todos = functions.get_todos()
+                todos.remove(todo_to_complete)
+                functions.write_todos(todos)
+                window['todos'].update(values=todos)
+                window['todo'].update(value='')
+            except IndexError:
+                sg.popup("Please select the item first", font=('Helvetica', 15))
         case "Exit":
             break
         case "todos":
-            window['todo'].update(value=values['todos'][0])
+            window['todo'].update(value=values['todos'][0].strip('\n'))
         case sg.WIN_CLOSED:
             break
 
